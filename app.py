5f8a1ff4962828d186758459b75c1c39ed130dcf
Joe Kadi <joe.kadi1@nhs.net>
2022-04-11 16:19:33 +0100

    extracting authors and sorting

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ from datetime import datetime
 import pytz
 import sys
 
+
+def sort_dict_by_value(d):
+    return dict(sorted(d.items(), key=lambda x: x[1], reverse=True))
+
+
 HEADERS = {'PRIVATE-TOKEN': sys.argv[1]}
+
+ready_to_merge = {'merge_requests': [], 'authors': {}}
+older_than_a_month = {'merge_requests': [], 'authors': {}}
+need_one_reviewer = {'merge_requests': [], 'authors': {}}
+need_two_reviewers = {'merge_requests': [], 'authors': {}}
 
 response_raw = requests.get(
     'http://git.spine2.ncrs.nhs.uk/api/v4/groups/304/merge_requests?&state=opened&scope=all', headers=HEADERS)
@@ -22,7 +32,8 @@ for page in range(int(response_raw.headers['X-Page'])+1, int(response_raw.header
         response.append(merge_request)
 
 if total_items == len(response):
-    print('Got correct number of MRs as ', len(response), '=', total_items)
+    print('Extracted the correct number of MRs as ',
+          len(response), '=', total_items)
 else:
     raise Exception('Got wrong number of MRs',
                     len(response), '!=', total_items)
@@ -31,30 +42,67 @@ response_string = json.dumps(response)
 with open('json_data.json', 'w') as outfile:
     outfile.write(response_string)
 
-ready_to_merge = []
-older_than_a_month = []
-need_one_reviewer = []
-need_two_reviewers = []
 for merge_request in response:
+    author = merge_request['author']['name']
 
     time_between_insertion = datetime.utcnow().replace(tzinfo=pytz.utc) - \
         parser.parse(merge_request['created_at'])
 
     if(time_between_insertion.days > 30):
-        older_than_a_month.append(merge_request)
+        older_than_a_month['merge_requests'].append(merge_request)
+        if author in older_than_a_month['authors']:
+            older_than_a_month['authors'][author] += 1
+        else:
+            older_than_a_month['authors'][author] = 1
         continue
 
-    if merge_request['upvotes'] >= 2:
-        ready_to_merge.append(merge_request)
+    if merge_request['upvotes'] == 0:
+        need_two_reviewers['merge_requests'].append(merge_request)
+        if author in need_two_reviewers['authors']:
+            need_two_reviewers['authors'][author] += 1
+        else:
+            need_two_reviewers['authors'][author] = 1
 
     if merge_request['upvotes'] == 1:
-        need_one_reviewer.append(merge_request)
+        need_one_reviewer['merge_requests'].append(merge_request)
+        if author in need_one_reviewer['authors']:
+            need_one_reviewer['authors'][author] += 1
+        else:
+            need_one_reviewer['authors'][author] = 1
 
-    if merge_request['upvotes'] == 0:
-        need_two_reviewers.append(merge_request)
+    if merge_request['upvotes'] >= 2:
+        ready_to_merge['merge_requests'].append(merge_request)
+        if author in ready_to_merge['authors']:
+            ready_to_merge['authors'][author] += 1
+        else:
+            ready_to_merge['authors'][author] = 1
 
+print(older_than_a_month['merge_requests'][0])
 
-print('Older than a month: ', len(older_than_a_month))
-print('Ready to merge ', len(ready_to_merge))
-print('Need 1 reviewer ', len(need_one_reviewer))
-print('Need 2 reviewer ', len(need_two_reviewers))
+older_than_a_month['authors'] = sort_dict_by_value(
+    older_than_a_month['authors'])
+
+ready_to_merge['authors'] = sort_dict_by_value(ready_to_merge['authors'])
+
+need_one_reviewer['authors'] = sort_dict_by_value(
+    need_one_reviewer['authors'])
+
+need_two_reviewers['authors'] = sort_dict_by_value(
+    need_two_reviewers['authors'])
+
+print('Older than a month: ', len(older_than_a_month['merge_requests']))
+
+# for author, count in older_than_a_month['authors'].items():
+#     print(author, ':', count)
+
+print('Ready to merge ', len(ready_to_merge['merge_requests']))
+# for author, count in ready_to_merge['authors'].items():
+#     print(author, ':', count)
+
+print('Need 1 reviewer ', len(need_one_reviewer['merge_requests']))
+# for author, count in need_one_reviewer['authors'].items():
+#     print(author, ':', count)
+
+print('Need 2 reviewer ', len(need_two_reviewers['merge_requests']))
+# for author, count in need_two_reviewers['authors'].items():
+#     print(author, ':', count)
